3ddeed4e3aeade53bcf666dccc72fb47620ab8ee
Meshkat <meshkat.hashemian@yahoo.com>
2023-09-27 19:27:29 +0330

    Users Class Added

---
+++ src/main/java/entity/Users.java
@@ -0,0 +1,80 @@
+package entity;
+
+@SuppressWarnings("unused")
+
+public class Users {
+    private int userId;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+
+    public Users(int userId, String name, String userName, String email, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Users(String name, String userName, String email, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Users() {
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
